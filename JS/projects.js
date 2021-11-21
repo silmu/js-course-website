cbc714d5f01c6a5e546440aa3c898d21cd8d06ad
Misori <svetlana.raitina@gmail.com>
2021-11-21 02:36:12 +0200

    Make function anonymous.

--- JS/projects.js
+++ JS/projects.js
@@ -1,21 +1,21 @@
 //mouse over the 1st project even
 var project1 = document.getElementById('project1');
-project1.addEventListener('mouseover', (event) => {
-    if (event.target !== event.currentTarget)
-    return;
-    event.target.style.backgroundColor = "rgb(231, 231, 231)";
-    setTimeout(function() {
-        event.target.style.backgroundColor = "";
-    }, 1000);
-}, false);
+project1.addEventListener('mouseover', function (event) {
+        if (event.target !== event.currentTarget)
+            return;
+        event.target.style.backgroundColor = "rgb(231, 231, 231)";
+        setTimeout(function () {
+            event.target.style.backgroundColor = "";
+        }, 1000);
+    }, false);
 //event on load
-window.addEventListener('load', (event) => {
+window.addEventListener('load', function (event) {
         alert('The page is loaded!');
     });
 //event on scroll
 var i = 0;
-window.addEventListener('scroll', (event) => {
+window.addEventListener('scroll', function (event) {
         i++;
-        document.getElementById('scrolls').innerHTML= i;
+        document.getElementById('scrolls').innerHTML = i;
 
     });
