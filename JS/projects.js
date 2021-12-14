f05c2dd6256df110cab2e5c18823e1693c800cba
Misori <svetlana.raitina@gmail.com>
2021-12-15 00:32:23 +0200

    Change a DOM command to jQuery command

--- JS/projects.js
+++ JS/projects.js
@@ -1,6 +1,9 @@
-//mouse over the 1st project even
-var project1 = document.getElementById('project1');
-project1.addEventListener('mouseover', function (event) {
+// Change color of a project on mouseover
+//Loop to add eventListener on each project
+for(var i = 1; i <= 3; i++){
+    var project = $('#project'+ i)[0];
+    // Function to change color
+    project.addEventListener('mouseover', function (event) {
         if (event.target !== event.currentTarget)
             return;
         event.target.style.backgroundColor = "rgb(231, 231, 231)";
@@ -8,7 +11,10 @@ project1.addEventListener('mouseover', function (event) {
             event.target.style.backgroundColor = "";
         }, 1000);
     }, false);
-//event on scroll
+}
+
+
+// Count how many times the page is scrolled
 var i = 0;
 window.addEventListener('scroll', function (event) {
         i++;
